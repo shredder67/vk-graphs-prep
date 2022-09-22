60e6c492cc82e5f4a03574ccb650457ddda3fe6b
[Sergey K.] <seriy.karp2@gmail.com>
2022-09-23 02:26:40 +0300

    Some general architecture pipeline created. Tested some configureations of GNN block.

--- models/common.py
+++ models/common.py
@@ -23,31 +23,31 @@ def train_model(
         device=torch.device('cuda')):
     
     model.train()
-    accuracy = Accuracy().to(device)
+    f1score = F1Score().to(device) 
     loss_hist = []
-    acc_hist = []
-    acc_val_hist = []
-    with tqdm(range(epochs)) as tepoch:
+    f1_hist = []
+    f1_val_hist = []
+    with tqdm(range(1, epochs + 1)) as tepoch:
         for e in tepoch:
             optimizer.zero_grad()
             out = model(data).squeeze(-1)
-            preds = torch.zeros(size=out.shape, dtype=torch.int, device=device)
-            preds[out > 0.5] = 1
+            preds = torch.sigmoid(out)
             loss = criterion(out[data.train_mask], data.y[data.train_mask])
             loss.backward()
             optimizer.step()
 
             loss_hist.append(loss.item())
-            acc_hist.append(accuracy(preds[data.train_mask], data.y[data.train_mask].to(torch.int)).item())
+            f1_hist.append(f1score(preds[data.train_mask], data.y[data.train_mask].to(torch.int)).item())
 
-            if log_stats and e%eval_freq == 0:
-                acc_val_hist.append(
-                    eval_model(model, data, {"acc": accuracy}, device=device)["acc"]
+            if e%eval_freq == 0:
+                f1_val_hist.append(
+                    eval_model(model, data, {"acc": f1score}, device=device)["acc"]
                 )
-                tepoch.write(f'\nepoch {e}:\ntr_loss {loss_hist[-1]:.4f}\ntr_acc: {acc_hist[-1]:.4f} \
-                                \nval_acc: {acc_val_hist[-1]:.4f}')
+                if log_stats:
+                    tepoch.write(f'\nepoch {e}:\ntr_loss {loss_hist[-1]:.4f}\ntr_f1: {f1_hist[-1]:.4f} \
+                                    \nval_f1: {f1_val_hist[-1]:.4f}')
 
-    return loss_hist, acc_hist, acc_val_hist
+    return loss_hist, f1_hist, f1_val_hist
 
 
 @torch.no_grad()
@@ -55,10 +55,9 @@ def eval_model(model, data, metrics: dict, use_val_mask=True, device=torch.devic
     model.eval()
     mask = data.val_mask if use_val_mask else data.test_mask
     out = model(data).squeeze(-1)
-    preds = torch.zeros(size=out.shape, dtype=torch.int, device=device)
+    preds = torch.sigmoid(out)
     y_true = data.y[mask].to(torch.int)
-    preds[out > 0.5] = 1
-    metric_values = dict.fromkeys(metrics.keys())
+    metric_values = dict()
     for metric_key, metric in metrics.items():
         metric_values[metric_key] = metric(preds[mask], y_true).item()
     return metric_values
@@ -67,10 +66,10 @@ def eval_model(model, data, metrics: dict, use_val_mask=True, device=torch.devic
 def plot_hist(train_loss_hist, train_acc_hist, eval_acc_hist, eval_freq):
     _, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5))
     ax1.plot(train_loss_hist, label='train loss')
-    ax1.title('Training Loss')
-    ax2.plot(train_acc_hist, label='train accuracy')
-    ax2.plot(range(0, len(train_acc_hist), eval_freq), eval_acc_hist, label='eval accuracy')
-    ax2.title('Training and Validation Accuracy')
+    ax1.set_title('Training Loss')
+    ax2.plot(train_acc_hist, label='train f1-score')
+    ax2.plot(list(range(0, len(train_acc_hist), eval_freq)), eval_acc_hist, label='eval f1-score')
+    ax2.set_title('Training and Validation Metric (F1-Score)')
     plt.legend()
     plt.show()
 
@@ -81,12 +80,11 @@ def benchmark_model(model, data, apply_val_mask=False, apply_test_mask=True, met
 
     if metrics is None:
         metrics = {
-            'acc': Accuracy().to(device),
             'recall': Recall().to(device),
             'precision': Precision().to(device),
             'f1-score': F1Score().to(device) 
         }
-
+    
     return eval_model(model, data, metrics, apply_val_mask, device)
     
 
@@ -135,3 +133,25 @@ def plot_subgraph(data: torch_geometric.data.Data, vert_list: List[int]):
     subgr_edge_index, subgr_edge_attr = subgraph(vert_list, data.edge_index, data.edge_attr)
     subgraph_data = torch_geometric.data.Data(edge_index=subgr_edge_index, edge_attr=subgr_edge_attr)
     plot_graph(subgraph_data)
+
+
+def plot_features_distributions(data: torch_geometric.data.Data, vert_list: List[int], plot_type='box'):
+    n = int(len(vert_list)**0.5)
+    k = len(vert_list) // n
+    if n*k < len(vert_list):
+        k += 1
+    fig, axs = plt.subplots(n, k)
+    for i in range(n):
+        for j in range(k):
+            if i*k + j == len(vert_list):
+                break
+            if plot_type == 'hist':
+                axs[i, j].hist(data.x[:, vert_list[i*k + j]])
+            elif plot_type == 'violin':
+                axs[i, j].violinplot(data.x[:, vert_list[i*k + j]])
+            elif plot_type == 'box':
+                axs[i, j].boxplot(data.x[:, vert_list[i*k + j]])
+
+    plt.tight_layout()
+    plt.show()
+
